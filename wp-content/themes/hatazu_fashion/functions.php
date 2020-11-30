c4222d632a997565a6f166f78c1ec777b0ab34d5
hatazu <hatazu@gmail.com>
2020-11-30 12:31:15 +0700

    col archie

--- wp-content/themes/hatazu_fashion/functions.php
+++ wp-content/themes/hatazu_fashion/functions.php
@@ -246,9 +246,11 @@ add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 function new_loop_shop_per_page( $cols ) {
   // $cols contains the current number of products per page based on the value stored on Options -> Reading
   // Return the number of products you wanna show per page.
-  $cols = 9;
+  $cols = 10;
   return $cols;
 }
+
+
 function ld_custom_excerpt_length( $length ) {
     return 15;
 }
